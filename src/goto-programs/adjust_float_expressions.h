6f40ad1883c77ca8d269441ad7b5b451ae47e41e
Hannes Steffenhagen <hannes.steffenhagen@diffblue.com>
2018-04-17 13:27:58 +0100

    Move adjust_float_expressions to goto-programs

--- src/goto-programs/adjust_float_expressions.h
+++ src/goto-programs/adjust_float_expressions.h
@@ -9,18 +9,16 @@ Author: Daniel Kroening, kroening@kroening.com
 /// \file
 /// Symbolic Execution
 
-#ifndef CPROVER_GOTO_SYMEX_ADJUST_FLOAT_EXPRESSIONS_H
-#define CPROVER_GOTO_SYMEX_ADJUST_FLOAT_EXPRESSIONS_H
+#ifndef CPROVER_GOTO_PROGRAMS_ADJUST_FLOAT_EXPRESSIONS_H
+#define CPROVER_GOTO_PROGRAMS_ADJUST_FLOAT_EXPRESSIONS_H
 
-#include <goto-programs/goto_functions.h>
+#include "goto_functions.h"
 
 class exprt;
 class namespacet;
 class goto_modelt;
 
-void adjust_float_expressions(
-  exprt &expr,
-  const namespacet &ns);
+void adjust_float_expressions(exprt &expr, const namespacet &ns);
 
 void adjust_float_expressions(
   goto_functionst::goto_functiont &goto_function,
@@ -29,6 +27,7 @@ void adjust_float_expressions(
 void adjust_float_expressions(
   goto_functionst &goto_functions,
   const namespacet &ns);
+
 void adjust_float_expressions(goto_modelt &goto_model);
 
-#endif // CPROVER_GOTO_SYMEX_ADJUST_FLOAT_EXPRESSIONS_H
+#endif // CPROVER_GOTO_PROGRAMS_ADJUST_FLOAT_EXPRESSIONS_H
--- src/jbmc/jbmc_parse_options.cpp
+++ src/jbmc/jbmc_parse_options.cpp
@@ -43,8 +43,7 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <goto-programs/show_goto_functions.h>
 #include <goto-programs/show_symbol_table.h>
 #include <goto-programs/show_properties.h>
-
-#include <goto-symex/adjust_float_expressions.h>
+#include <goto-programs/adjust_float_expressions.h>
 
 #include <goto-instrument/full_slicer.h>
 #include <goto-instrument/reachability_slicer.h>
