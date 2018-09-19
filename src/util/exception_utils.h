ab71e5d78f9ae613b17b8a6795f222ff8b782090
Hannes Steffenhagen <hannes.steffenhagen@diffblue.com>
2018-09-19 16:51:48 +0100

    Add additional exception classes and introduce base exception
    This adds a number of new exception classes that are meant to be used to replace
    unstructured throws in other parts of the code. The exception types have been
    selected according to what we already determined we are going to need.
    
    Further, this has a slight fix in the format for invalid_user_input_exceptiont
    (it did previously not add a separator between the reason and the suggestion on
    how to fix the error).
    
    This also adds a cprover_exception_baset which is mean to avoid unnecessary
    overhead when adding new exception types - without it, all driver programs would
    have to updated to explicitly catch the new exception type whenever we introduce
    one.
    
    Also removes some dead code parse_options_baset (there was a final return that
    cannot be reached because all ancestor statements are returns).

--- src/util/exception_utils.h
+++ src/util/exception_utils.h
@@ -11,13 +11,24 @@ Author: Fotis Koutoulakis, fotis.koutoulakis@diffblue.com
 
 #include <string>
 
-class cprover_exceptiont
+#include "source_location.h"
+
+/// Base class for exceptions thrown in the cprover project.
+/// Intended to be used as a convenient way to have a
+/// "catch all and report errors" from application entry points.
+class cprover_exception_baset
 {
 public:
-  virtual std::string what() const noexcept = 0;
+  /// A human readable description of what went wrong.
+  /// For readability, implementors should not add a leading
+  /// or trailing newline to this description.
+  virtual std::string what() const = 0;
 };
 
-class invalid_user_input_exceptiont : public cprover_exceptiont
+/// Thrown when users pass incorrect command line arguments,
+/// for example passing no files to analysis or setting
+/// two mutually exclusive flags
+class invalid_user_input_exceptiont : public cprover_exception_baset
 {
   /// The reason this exception was generated.
   std::string reason;
@@ -31,20 +42,68 @@ public:
   invalid_user_input_exceptiont(
     std::string reason,
     std::string option,
-    std::string correct_input = "")
-    : reason(reason), option(option), correct_input(correct_input)
-  {
-  }
+    std::string correct_input = "");
 
-  std::string what() const noexcept override;
+  std::string what() const override;
 };
 
-class io_exceptiont : cprover_exceptiont
+/// Thrown when some external system fails unexpectedly.
+/// Examples are IO exceptions (files not present, or we don't
+/// have the right permissions to interact with them), timeouts for
+/// external processes etc
+class system_exceptiont : public cprover_exception_baset
 {
 public:
-  explicit io_exceptiont(std::string message);
-  std::string what() const noexcept override;
+  explicit system_exceptiont(std::string message);
+  std::string what() const override;
+
 private:
+  std::string message;
+};
+
+/// Thrown when failing to deserialize a value from some
+/// low level format, like JSON or raw bytes
+class deserialization_exceptiont : public cprover_exception_baset
+{
+public:
+  explicit deserialization_exceptiont(std::string message);
+
+  std::string what() const override;
+
+private:
+  std::string message;
+};
+
+/// Thrown when a goto program that's being processed is in an invalid format,
+/// for example passing the wrong number of arguments to functions.
+/// Note that this only applies to goto programs that are user provided,
+/// that internal transformations on goto programs don't produce invalid
+/// programs should be guarded by invariants instead.
+/// \see invariant.h
+class incorrect_goto_program_exceptiont : public cprover_exception_baset
+{
+public:
+  incorrect_goto_program_exceptiont(
+    std::string message,
+    source_locationt source_location);
+  std::string what() const override;
+
+private:
+  std::string message;
+  source_locationt source_location;
+};
+
+/// Thrown when we encounter an instruction, parameters to an instruction etc.
+/// in a goto program that has some theoretically valid semantics,
+/// but that we don't presently have any support for.
+class unsupported_operation_exceptiont : public cprover_exception_baset
+{
+public:
+  explicit unsupported_operation_exceptiont(std::string message);
+  std::string what() const override;
+
+private:
+  /// The unsupported operation causing this fault to occur.
   std::string message;
 };
 
