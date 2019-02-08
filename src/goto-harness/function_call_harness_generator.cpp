de1e67452f9f880b9fd22e1661a86f439400ff07
Fotis Koutoulakis <fotis.koutoulakis@diffblue.com>
2019-02-08 11:40:31 +0000

    [WIP] it works

--- src/goto-harness/function_call_harness_generator.cpp
+++ src/goto-harness/function_call_harness_generator.cpp
@@ -23,6 +23,18 @@ struct function_call_harness_generatort::implt
 {
   ui_message_handlert *message_handler;
   irep_idt function;
+  irep_idt harness_function_name;
+  symbol_tablet *symbol_table;
+  goto_functionst *goto_functions;
+  bool nondet_globals = false;
+
+  void generate(goto_modelt &goto_model, const irep_idt &harness_function_name);
+  void generate_nondet_globals(code_blockt &function_body);
+  void setup_parameters_and_call_entry_function(code_blockt &function_body);
+  const symbolt &lookup_function_to_call();
+  void generate_initialisation_code_for(code_blockt &block, const exprt &lhs);
+  void ensure_harness_does_not_already_exist();
+  void add_harness_function_to_goto_model(code_blockt function_body);
 };
 
 function_call_harness_generatort::function_call_harness_generatort(
@@ -42,6 +54,10 @@ void function_call_harness_generatort::handle_option(
   {
     p_impl->function = require_exactly_one_value(option, values);
   }
+  else if (option == FUNCTION_HARNESS_GENERATOR_NONDET_GLOBALS_OPT)
+  {
+    p_impl->nondet_globals = true;
+  }
   else
   {
     throw invalid_command_line_argument_exceptiont{
@@ -53,76 +69,75 @@ void function_call_harness_generatort::generate(
   goto_modelt &goto_model,
   const irep_idt &harness_function_name)
 {
-  auto const &function = p_impl->function;
-  auto &symbol_table = goto_model.symbol_table;
-  auto function_found = symbol_table.lookup(function);
-  auto harness_function_found = symbol_table.lookup(harness_function_name);
+  p_impl->generate(goto_model, harness_function_name);
+}
 
-  if(function_found == nullptr)
-  {
-    throw invalid_command_line_argument_exceptiont{
-      "function that should be harnessed is not found " + id2string(function),
-      "--" FUNCTION_HARNESS_GENERATOR_FUNCTION_OPT};
-  }
-
-  if(harness_function_found != nullptr)
-  {
-    throw invalid_command_line_argument_exceptiont{
-      "harness function already in the symbol table " +
-        id2string(harness_function_name),
-      "--" GOTO_HARNESS_GENERATOR_HARNESS_FUNCTION_NAME_OPT};
-  }
-
-  auto allocate_objects = allocate_objectst{function_found->mode,
-                                            function_found->location,
-                                            "__goto_harness",
-                                            symbol_table};
-
-  // create body for the function
-  code_blockt function_body{};
-
-  const auto &function_type = to_code_type(function_found->type);
+void function_call_harness_generatort::implt::setup_parameters_and_call_entry_function(
+  code_blockt &function_body)
+{
+  const auto &function_to_call = lookup_function_to_call();
+  const auto &function_type = to_code_type(function_to_call.type);
   const auto &parameters = function_type.parameters();
 
   code_function_callt::operandst arguments{};
-  arguments.reserve(parameters.size());
 
+  auto allocate_objects =
+    allocate_objectst{function_to_call.mode, function_to_call.location, "__goto_harness",
+                      *symbol_table};
   for(const auto &parameter : parameters)
   {
     auto argument = allocate_objects.allocate_automatic_local_object(
       parameter.type(), parameter.get_base_name());
-    arguments.push_back(std::move(argument));
+    generate_initialisation_code_for(function_body, argument);
+    arguments.push_back(argument);
   }
 
-  code_function_callt function_call{function_found->symbol_expr(),
-                                    std::move(arguments)};
-  function_call.add_source_location() = function_found->location;
+  code_function_callt function_call{function_to_call.symbol_expr(), std::move(arguments)};
+  function_call.add_source_location() = function_to_call.location;
 
   function_body.add(std::move(function_call));
+}
 
-  // create the function symbol
-  symbolt harness_function_symbol{};
-  harness_function_symbol.name = harness_function_symbol.base_name =
-    harness_function_symbol.pretty_name = harness_function_name;
+void function_call_harness_generatort::implt::generate(
+  goto_modelt &goto_model,
+  const irep_idt &harness_function_name)
+{
+  symbol_table = &goto_model.symbol_table;
+  goto_functions = &goto_model.goto_functions;
+  this->harness_function_name = harness_function_name;
+  ensure_harness_does_not_already_exist();
 
-  harness_function_symbol.is_lvalue = true;
-  harness_function_symbol.mode = function_found->mode;
-  harness_function_symbol.type = code_typet{{}, empty_typet{}};
-  harness_function_symbol.value = function_body;
+  // create body for the function
+  code_blockt function_body{};
 
-  symbol_table.insert(harness_function_symbol);
+  generate_nondet_globals(function_body);
+  setup_parameters_and_call_entry_function(function_body);
+  add_harness_function_to_goto_model(std::move(function_body));
+}
 
-  goto_model.goto_functions.function_map[harness_function_name].type =
-    to_code_type(harness_function_symbol.type);
-  auto &body =
-    goto_model.goto_functions.function_map[harness_function_name].body;
-  goto_convert(
-    static_cast<const codet &>(harness_function_symbol.value),
-    goto_model.symbol_table,
-    body,
-    *p_impl->message_handler,
-    function_found->mode);
-  body.add(goto_programt::make_end_function());
+void function_call_harness_generatort::implt::generate_nondet_globals(
+  code_blockt &function_body)
+{
+  if(nondet_globals)
+  {
+    for(const auto &symbol_table_entry : *symbol_table)
+    {
+      const auto &symbol = symbol_table_entry.second;
+      if(
+        symbol.is_static_lifetime && symbol.is_lvalue &&
+        !has_prefix(id2string(symbol.name), "__CPROVER_"))
+      {
+        generate_initialisation_code_for(function_body, symbol.symbol_expr());
+      }
+    }
+  }
+}
+
+void function_call_harness_generatort::implt::generate_initialisation_code_for(
+  code_blockt &block,
+  const exprt &lhs)
+{
+  block.add(code_assignt{lhs, side_effect_expr_nondett(lhs.type())});
 }
 
 void function_call_harness_generatort::validate_options()
@@ -131,4 +146,61 @@ void function_call_harness_generatort::validate_options()
     throw invalid_command_line_argument_exceptiont{
       "required parameter entry function not set",
       "--" FUNCTION_HARNESS_GENERATOR_FUNCTION_OPT};
+}
+
+const symbolt &function_call_harness_generatort::implt::lookup_function_to_call()
+{
+  auto function_found = symbol_table->lookup(function);
+
+  if(function_found == nullptr)
+  {
+    throw invalid_command_line_argument_exceptiont{
+      "function that should be harnessed is not found " + id2string(function),
+      "--" FUNCTION_HARNESS_GENERATOR_FUNCTION_OPT};
+  }
+
+  return *function_found;
+}
+
+void function_call_harness_generatort::implt::ensure_harness_does_not_already_exist()
+{
+  if(symbol_table->lookup(harness_function_name))
+  {
+    throw invalid_command_line_argument_exceptiont{
+      "harness function already exists in the symbol table",
+      "--" GOTO_HARNESS_GENERATOR_HARNESS_FUNCTION_NAME_OPT};
+  }
+}
+
+void function_call_harness_generatort::implt::add_harness_function_to_goto_model(
+  code_blockt function_body)
+{
+  const auto &function_to_call = symbol_table->lookup_ref(function);
+
+  // create the function symbol
+  symbolt harness_function_symbol{};
+  harness_function_symbol.name = harness_function_symbol.base_name =
+  harness_function_symbol.pretty_name = harness_function_name;
+
+  harness_function_symbol.is_lvalue = true;
+  harness_function_symbol.mode = function_to_call.mode;
+  harness_function_symbol.type =
+    code_typet{{}, empty_typet{}};
+  harness_function_symbol.value =std::move(function_body);
+
+  symbol_table->insert(harness_function_symbol);
+
+  auto const &generated_harness =
+    symbol_table->lookup_ref(harness_function_name);
+  goto_functions->function_map[harness_function_name].type =
+    to_code_type(generated_harness.type);
+  auto &body =
+    goto_functions->function_map[harness_function_name].body;
+  goto_convert(
+    static_cast<const codet &>(generated_harness.value),
+    *symbol_table,
+    body,
+    *message_handler,
+    ID_C);
+  body.add_instruction(goto_program_instruction_typet::END_FUNCTION);
 }
--- src/goto-harness/function_harness_generator_options.h
+++ src/goto-harness/function_harness_generator_options.h
@@ -10,8 +10,15 @@ Author: Diffblue Ltd.
 #define CPROVER_GOTO_HARNESS_FUNCTION_HARNESS_GENERATOR_OPTIONS_H
 
 #define FUNCTION_HARNESS_GENERATOR_FUNCTION_OPT "function"
+#define FUNCTION_HARNESS_GENERATOR_NONDET_GLOBALS_OPT "nondet-globals"
+#define FUNCTION_HARNESS_GENERATOR_MIN_NULL_TREE_DEPTH_OPT "min-null-tree-depth"
+#define FUNCTION_HARNESS_GENERATOR_MAX_NONDET_TREE_DEPTH_OPT \
+  "max-nondet-tree-depth"
 #define FUNCTION_HARNESS_GENERATOR_OPTIONS                                     \
-  "(" FUNCTION_HARNESS_GENERATOR_FUNCTION_OPT "):"
+  "(" FUNCTION_HARNESS_GENERATOR_FUNCTION_OPT "):"                             \
+  "(" FUNCTION_HARNESS_GENERATOR_NONDET_GLOBALS_OPT ")"                        \
+  "(" FUNCTION_HARNESS_GENERATOR_MIN_NULL_TREE_DEPTH_OPT "):"                       \
+  "(" FUNCTION_HARNESS_GENERATOR_MAX_NONDET_TREE_DEPTH_OPT "):"                   \
 // FUNCTION_HARNESS_GENERATOR_OPTIONS
 
 // clang-format off
@@ -19,6 +26,15 @@ Author: Diffblue Ltd.
   "function harness generator (--harness-type call-function)\n\n"              \
   "--" FUNCTION_HARNESS_GENERATOR_FUNCTION_OPT                                 \
   "      the function the harness should call\n"                               \
+  "--" FUNCTION_HARNESS_GENERATOR_NONDET_GLOBALS_OPT                           \
+  "      set global variables to non-deterministic values in harness\n"        \
+  "--" FUNCTION_HARNESS_GENERATOR_MIN_NULL_TREE_DEPTH_OPT                      \
+  " --min-null-tree-depth N      minimum level at which a pointer can first be\n" /* NOLINT(*) */\
+  "                              NULL in a recursively nondet initialized " \
+  "struct\n" /* NOLINT(*) */ \
+  "--" FUNCTION_HARNESS_GENERATOR_MAX_NONDET_TREE_DEPTH_OPT                    \
+  " --max-nondet-tree-depth N    limit size of nondet (e.g. input) object tree;\n" /* NOLINT(*) */\
+  "                              at level N pointers are set to null\n" \
   // FUNCTION_HARNESS_GENERATOR_HELP
 // clang-format on
 
