23963046e82bbd697a28b78e8f44c04371314a32
Hannes Steffenhagen <hannes.steffenhagen@diffblue.com>
2019-02-19 10:48:42 +0000

    [WIP] start hooking up associated-array-size option

--- src/goto-harness/function_harness_generator_options.h
+++ src/goto-harness/function_harness_generator_options.h
@@ -18,6 +18,8 @@ Author: Diffblue Ltd.
 #define FUNCTION_HARNESS_GENERATOR_MAX_ARRAY_SIZE_OPT "max-array-size"
 #define FUNCTION_HARNESS_GENERATOR_TREAT_POINTER_AS_ARRAY_OPT                  \
   "treat-pointer-as-array"
+#define FUNCTION_HARNESS_GENERATOR_ASSOCIATED_ARRAY_SIZE_OPT                   \
+  "associated-array-size"
 
 // clang-format off
 #define FUNCTION_HARNESS_GENERATOR_OPTIONS                                     \
